0e3d6a5338ae66484bc77dbc5fd3b33b924bdcbe
KhulisoJohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-07-14 10:47:06 +0200

    new change

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764810aee94cf83f0057b05caac67942e45b0c25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df258a5062b26f789d5cd9519738817e5611314")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
