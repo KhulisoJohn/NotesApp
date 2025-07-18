0747e5cb3d4456b022efbc9e6d698847000e1885
KhulisoJohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-07-18 11:36:48 +0200

    initial commit

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4a47d93b68907e5ad1d9c32ed5ce26c5fba06c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+231bd9e21892bdc310dacd6a2ba5b93cebe00e98")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
